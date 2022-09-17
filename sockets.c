88e18f7779a7e6bcfc8cc0d946a42bd19e9f575e
Priyan Rajamohan <rajamohanpriyan@gmail.com>
2022-09-17 18:12:59 -0400

    getopt can receive arguments and able to print out

--- sockets.c
+++ sockets.c
@@ -1,76 +1,91 @@
 
+#include <netdb.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <strings.h>
 #include <string.h>
-#include <netdb.h>
-#include <unistd.h>
+#include <strings.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
+#include <unistd.h>
 
 #define ERROR 1
-#define REQUIRED_ARGC 3
+#define REQUIRED_ARGC 5
 #define HOST_POS 1
 #define PORT_POS 2
 #define PROTOCOL "tcp"
 #define BUFLEN 1024
 
-int usage (char *progname)
-{
-    fprintf (stderr,"usage: %s host port\n", progname);
-    exit (ERROR);
+int usage(char *progname) {
+    fprintf(stderr, "usage: %s host port\n", progname);
+    exit(ERROR);
 }
 
-int errexit (char *format, char *arg)
-{
-    fprintf (stderr,format,arg);
-    fprintf (stderr,"\n");
-    exit (ERROR);
+int errexit(char *format, char *arg) {
+    fprintf(stderr, format, arg);
+    fprintf(stderr, "\n");
+    exit(ERROR);
 }
 
-int main (int argc, char *argv [])
-{
+int main(int argc, char *argv[]) {
     struct sockaddr_in sin;
     struct hostent *hinfo;
     struct protoent *protoinfo;
-    char buffer [BUFLEN];
-    int sd, ret;
+    char buffer[BUFLEN];
+    int sd, ret, opt;
 
-    if (argc != REQUIRED_ARGC)
-        usage (argv [0]);
+    if (argc < REQUIRED_ARGC)
+        usage(argv[0]);
 
-    /* lookup the hostname */
-    hinfo = gethostbyname (argv [HOST_POS]);
-    if (hinfo == NULL)
-        errexit ("cannot find name: %s", argv [HOST_POS]);
+    while ((opt = getopt(argc, argv, "u:o:")) != -1) {
+        switch (opt) {
+            case 'o':
+                printf("filename %s\n", optarg);
+                break;
+            case 'u':
+                printf("url: %s\n", optarg);
+                break;
+            case '?':
+                if (optopt == 'u' || optopt == 'o') {
+                    printf("Missing mandatory option\n");
+                } else {
+                    printf("Invalid option received\n");
+                }
+                break;
+        }
+    }
 
-    /* set endpoint information */
-    memset ((char *)&sin, 0x0, sizeof (sin));
-    sin.sin_family = AF_INET;
-    sin.sin_port = htons (atoi (argv [PORT_POS]));
-    memcpy ((char *)&sin.sin_addr,hinfo->h_addr,hinfo->h_length);
+    // /* lookup the hostname */
+    // hinfo = gethostbyname(argv[HOST_POS]);
+    // if (hinfo == NULL)
+    //     errexit("cannot find name: %s", argv[HOST_POS]);
 
-    if ((protoinfo = getprotobyname (PROTOCOL)) == NULL)
-        errexit ("cannot find protocol information for %s", PROTOCOL);
+    // /* set endpoint information */
+    // memset((char *)&sin, 0x0, sizeof(sin));
+    // sin.sin_family = AF_INET;
+    // sin.sin_port = htons(atoi(argv[PORT_POS]));
+    // memcpy((char *)&sin.sin_addr, hinfo->h_addr, hinfo->h_length);
 
-    /* allocate a socket */
-    /*   would be SOCK_DGRAM for UDP */
-    sd = socket(PF_INET, SOCK_STREAM, protoinfo->p_proto);
-    if (sd < 0)
-        errexit("cannot create socket",NULL);
+    // if ((protoinfo = getprotobyname(PROTOCOL)) == NULL)
+    //     errexit("cannot find protocol information for %s", PROTOCOL);
 
-    /* connect the socket */
-    if (connect (sd, (struct sockaddr *)&sin, sizeof(sin)) < 0)
-        errexit ("cannot connect", NULL);
+    // /* allocate a socket */
+    // /*   would be SOCK_DGRAM for UDP */
+    // sd = socket(PF_INET, SOCK_STREAM, protoinfo->p_proto);
+    // if (sd < 0)
+    //     errexit("cannot create socket", NULL);
 
-    /* snarf whatever server provides and print it */
-    memset (buffer,0x0,BUFLEN);
-    ret = read (sd,buffer,BUFLEN - 1);
-    if (ret < 0)
-        errexit ("reading error",NULL);
-    fprintf (stdout,"%s\n",buffer);
-            
-    /* close & exit */
-    close (sd);
-    exit (0);
+    // /* connect the socket */
+    // if (connect(sd, (struct sockaddr *)&sin, sizeof(sin)) < 0)
+    //     errexit("cannot connect", NULL);
+
+    // /* snarf whatever server provides and print it */
+    // memset(buffer, 0x0, BUFLEN);
+    // ret = read(sd, buffer, BUFLEN - 1);
+    // if (ret < 0)
+    //     errexit("reading error", NULL);
+    // fprintf(stdout, "%s\n", buffer);
+
+    // /* close & exit */
+    // close(sd);
+    // exit(0);
 }
