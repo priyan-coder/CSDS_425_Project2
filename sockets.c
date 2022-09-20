c39a803392196a61ad9cfc1b9f127b6667613e00
Priyan Rajamohan <rajamohanpriyan@gmail.com>
2022-09-20 11:58:36 -0400

    this seems to concat the web_filename properly, further testing and rearrangement needed

--- sockets.c
+++ sockets.c
@@ -16,6 +16,7 @@
 #define PORT_POS 2
 #define PROTOCOL "tcp"
 #define BUFLEN 1024
+#define MAXSIZE 1024
 
 int usage(char *progname) {
     fprintf(stderr, "usage: %s -u URL [-i] [-c] [-s] -o filename\n", progname);
@@ -34,27 +35,34 @@ int main(int argc, char *argv[]) {
     struct protoent *protoinfo;
     char buffer[BUFLEN];
     int sd, ret, opt;
-    bool FILENAME_PARSED = false;
-    bool URL_PARSED = false;
-    char *url = NULL;
+    bool FILENAME_PARSED = false;  // flag to check if user entered filename argument after -o
+    bool URL_PARSED = false;       // flag to check if user entered URL argument after -u
+    char *url = NULL;              // to save the entire url which user enters on cmd line
+    char *HOSTNAME = NULL;         // hostname of the server
+    char *WEB_FILENAME = NULL;     // path to file in web server
+    char *OUTPUT_FILENAME = NULL;  // write to file in local system
+    bool PRINT_INFO = false;       // flag to check if user entered -i on the cmd line
 
     if (argc < REQUIRED_ARGC) {
         usage(argv[0]);
     }
 
     while (optind < argc) {
-        if ((opt = getopt(argc, argv, ":u:o:")) != -1) {
+        if ((opt = getopt(argc, argv, ":u:o:i")) != -1) {
             switch (opt) {
                 case 'o':
                     FILENAME_PARSED = true;
-                    printf("filename: %s\n", optarg);
+                    // printf("filename: %s\n", optarg);
                     break;
                 case 'u':
                     URL_PARSED = true;
                     // printf("%lu", strlen(optarg));
                     url = optarg;
-                    printf("url: %s\n", optarg);
+                    // printf("url: %s\n", optarg);
                     break;
+                case 'i':
+                    // PRINT INFO
+                    PRINT_INFO = true;
                 case '?':
                     printf("Unknown option: %c\n", optopt);
                     break;
@@ -73,11 +81,13 @@ int main(int argc, char *argv[]) {
     }
 
     /* Convert to lowercase */
-    char *endpoint = malloc(strlen(url) * sizeof(char));
+    char *endpoint = malloc((sizeof(url) + 1) * sizeof(char));
     strcpy(endpoint, url);
     for (int i = 0; i < strlen(url); i++) {
         endpoint[i] = tolower(endpoint[i]);
+        // printf("%c\n", endpoint[i]);
     }
+    // printf("%c\n", endpoint[strlen(url) - 1]);
 
     /* Check for Http only */
     char *token = strtok(endpoint, "/");
@@ -87,20 +97,58 @@ int main(int argc, char *argv[]) {
     }
 
     /* Tokenize the URL*/
-    char **info = malloc(sizeof(char) * 0);
+    // char **info = malloc(sizeof(char) * 0);
+    char *info[MAXSIZE];
     int i = 0;
     while (token != NULL) {
-        info = realloc(info, sizeof(char) * (i + 1));
+        // info = realloc(info, sizeof(char) * (i + 1));
         // printf("%s\n", token);
         info[i] = token;
         token = strtok(NULL, "/");
         i++;
     }
-    // printf("num of elems : %d\n", i);
+
+    // i indicates the number of elements or tokens in the info arr
+    /* A url needs to have http and hostname as a minimum */
+    // if (i < 2) {
+    //     printf("Please enter a valid URL!\n");
+    //     usage(argv[0]);
+    // }
+
+    printf("num of elems : %d\n", i);
     // while (i--) {
     //     printf("%s\n", info[i]);
     // }
 
+    // printf("%s\n", info[i]);
+    /* Set HOSTNAME and WEB_FILENAME */
+    HOSTNAME = info[1];  // info[0] contains http:
+    // // e.g. http://ll/mo/
+    // char *temp = malloc(sizeof(char) * (2 * i + 1));
+    // if (i == 2) {
+    //     temp = "/";
+    // } else {
+    //     for (int j = 0; j < i; j += 1) {
+    //         printf("Current elem: %s\n", info[i]);
+    //         strcat(temp, "/");
+    //         strcat(temp, info[i]);
+    //     }
+    //     strcat(temp, "/");
+    // }
+
+    // WEB_FILENAME = temp;
+    // printf("WEB_FILENAME : %s\n", WEB_FILENAME);
+    printf("HOSTNAME: %s\n", HOSTNAME);
+
+    char temp[MAXSIZE];
+    strcat(temp, "/");
+    for (int j = 2; j < i; j++) {
+        strcat(temp, info[j]);
+        strcat(temp, "/");
+    }
+    printf("temp: %s\n", temp);
+
+    // -----
     /* Get all of the non-option arguments */
     // if (optind < argc) {
     //     printf("Non-option args: ");
